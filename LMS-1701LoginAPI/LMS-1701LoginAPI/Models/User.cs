9e63f051ebfca7311f19678ab21e0e9a1c6065e8
em322609 <em322609@ohio.edu>
2017-03-22 13:46:27 -0400

    initial commit. contains Login Web Service  & DAL for that Web Service

---
+++ LMS-1701LoginAPI/LMS-1701LoginAPI/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LMS_1701LoginAPI.Models
+{
+    public class User
+    {
+        public User()
+        {
+
+        }
+
+        public User(int uid, string fn, string ln, int? ut, string e, string p)
+        {
+            this.UserId = uid;
+            this.Email = e;
+            this.Password = p;
+            this.FirstName = fn;
+            this.LastName = ln;
+            this.UserType = ut;
+            
+        }
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int? UserType { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
